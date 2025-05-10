1319b0676238132d86e22cf23446307cb6e600c8
toobi-jpeg <rex.98@live.se>
2025-05-10 15:16:32 +0200

    all input fields added & small design updates.

--- src/Components/FormInput.tsx
+++ src/Components/FormInput.tsx
@@ -4,19 +4,54 @@ interface FormInputProps {
   label: string;
   type: string;
   id: string;
+  options?: { label: string; value: string }[];
 }
 
-const FormInput: React.FC<FormInputProps> = ({ label, type, id }) => {
+const FormInput: React.FC<FormInputProps> = ({ label, type, id, options }) => {
   if (type === "checkbox") {
     return (
       <div className="flex w-full gap-x-2 items-center mt-3 ml-0.5">
         <label htmlFor={id} className="text-sm">
-          Are you employed?
+          {label}
         </label>
         <input
           id={id}
           type="checkbox"
           className="bg-bg2 border-1 rounded mt-1"
+        />
+      </div>
+    );
+  } else if (type === "select") {
+    return (
+      <div className="flex flex-col mt-2">
+        <div className="flex w-full text-left ml-0.5">
+          <label htmlFor={id} className="text-m">
+            {label}
+          </label>
+        </div>
+        <select
+          id={id}
+          className="flex items-center w-64 h-8 bg-bg2 border-1 border rounded outline-none px-2 text-sm"
+        >
+          {options?.map((option, index) => (
+            <option key={`${index}-${option.value}`} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  } else if (type === "textarea") {
+    return (
+      <div className="mt-2">
+        <div className="flex w-full text-left ml-0.5">
+          <label htmlFor={id} className="text-m">
+            {label}
+          </label>
+        </div>
+        <textarea
+          id={id}
+          className="w-64 h-16 bg-bg2 border-1 border rounded outline-none p-2 text-sm resize-none"
         />
       </div>
     );
--- src/Components/LoanForm.tsx
+++ src/Components/LoanForm.tsx
@@ -2,16 +2,31 @@ import FormInput from "./FormInput";
 
 export default function LoanForm() {
   return (
-    <div className="flex mx-auto max-w-sm items-center rounded-xl bg-bg p-6 border-1 border-light">
+    <div className="flex mx-auto max-w-sm items-center rounded-xl bg-bg p-6 card-shadow">
       <form action="loan-application" className="flex flex-col items-center">
-        <FormInput type="text" label="Name" id="name-input" />
-        <FormInput type="number" label="Phone" id="phone-input" />
-        <FormInput type="number" label="Age" id="age-input" />
+        <FormInput type="text" label="Namn" id="name-input" />
+        <FormInput type="number" label="Telefon" id="phone-input" />
+        <FormInput type="number" label="Ålder" id="age-input" />
         <FormInput
           type="checkbox"
-          label="Are you Employed?"
+          label="Är du anställd?"
           id="employed-input"
         />
+        <FormInput
+          type="select"
+          label="Din lön"
+          id="salary-input"
+          options={[
+            { label: "Mindre än 5000kr", value: "<5000" },
+            { label: "5000kr - 10 000kr", value: "5000-10000" },
+            { label: "10 000kr - 20 000kr", value: "10000-20000" },
+            { label: "Över 20 000kr", value: ">20000" },
+          ]}
+        />
+        <FormInput type="number" label="Lånebelopp" id="amount-input" />
+        <FormInput type="text" label="Syfte" id="purpose-input" />
+        <FormInput type="number" label="Återbetalningstid" id="period-input" />
+        <FormInput type="textarea" label="Kommentar" id="comment-input" />
       </form>
     </div>
   );
