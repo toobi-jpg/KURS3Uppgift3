d25ab6ae705ba5e730f6b68a8ff402b5da82cbfb
toobi-jpeg <rex.98@live.se>
2025-05-10 02:17:05 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      </div>
+    </>
+  );
+}
